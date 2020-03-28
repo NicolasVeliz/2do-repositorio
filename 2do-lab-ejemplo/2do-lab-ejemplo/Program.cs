5ea4c8bbba369292196761dd28957878274525de
Nicolas Veliz <niveliz@miuandes.cl>
2020-03-28 17:46:03 -0300

    primer commit

---
+++ 2do-lab-ejemplo/2do-lab-ejemplo/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2do_lab_ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
